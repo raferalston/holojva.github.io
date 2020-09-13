e420afabec7f6c8dbf75b59e3c2ce206d4fdff1e
holojva <64406968+holojva@users.noreply.github.com>
2020-09-13 18:19:32 +0300

    Add files via upload

--- h25.js
+++ h25.js
@@ -1,8 +1,10 @@
-while (true) {
+let boxik = document.querySelector('#boxik')
     function runEvent(event){
-           output.innerHTML = '<h3>MouseX ' + event.offsetX + ' </h3>' + '<h3>MouseY ' + event.offsetY + '</h3>';
-           let iks = event.offsetX * 10.0392156863;
-           let igrek = event.offsetY * 6.27450980392;
-           box.style.backgroundColor = 'rgb(' + iks  + ' , ' + igrek + ', 40)' 
+           let a = window.innerWidth / 255
+           let b = window.innerHeight / 255
+           let iks = event.offsetX * a;
+           let igrek = event.offsetY * b;
+           boxik.style.backgroundColor = 'rgb(' + iks  + ' , ' + igrek + ', 40)' 
       }
-}
+boxik.addEventListener('mousemove', runEvent)
+boxik.style.width = window.innerWidth; boxik.style.height = window.innerHeight;

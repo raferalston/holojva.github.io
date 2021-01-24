cd471ea51a327efb85e4a531bb7d9be0f3787d4f
holojva <64406968+holojva@users.noreply.github.com>
2021-01-24 18:03:46 +0300

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,33 @@
+otvet = "виселица"
+p_otvet = []
+for i in list(otvet) :
+    p_otvet += "_"
+p_otvet = "".join(p_otvet)    
+print(f"Это игра про виселицу, тут надо писать буквы, у вас 3 неправильных попытки, слово из {len(otvet)}. " )
+popytky = 3
+for i in range(len(otvet) + 3) :
+    bukva_naidena = False
+    bukva = -1
+    inp = input("Bukva: ")
+    for i in list(otvet):
+        bukva += 1
+        if inp == i :
+            bukva_naidena = True
+            p_otvet = list(p_otvet)
+            p_otvet[bukva] = inp
+            p_otvet = "".join(p_otvet)
+            print(p_otvet)
+        if otvet == p_otvet: 
+            pobeda = True
+            print(f"Вы ПОБЕДИЛИ!!!")
+            break 
+    if bukva_naidena == False:    
+        popytky -= 1
+        if popytky != 0 :
+            print(f"У вас осталось {popytky} попыток " )
+        else :
+            print("Ты проиграл(((")
+    if otvet == p_otvet :
+        break 
+
+            

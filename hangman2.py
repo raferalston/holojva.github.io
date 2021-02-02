8f3deb7251ac148822508cc9c92f9cb0fd8f8d95
holojva <64406968+holojva@users.noreply.github.com>
2021-02-02 19:07:24 +0300

    Add files via upload

--- hangman2.py
+++ hangman2.py
@@ -1,28 +1,64 @@
-def build_template(t, w, g, l) :
+def lose_speech() :
+    '''
+    output: None, it's used just built-in print() function
+    '''
+    print("Ты проиграл!")
+def win_speech() :
+    '''
+    output: None, it's used just built-in print() function
+    '''
+    print("Ты выиграл!")
+def check_attempt(life) :
     """
-    input: t - template (list), w - word (string) g - guess (string) l - lifes (int)
-    output: t - template (list) with replaced characters in template, lifes
+    input: life (int)
+    output: int, life -= 1
+    """
+   
+    life = life - 1
+    return life
+def check_mistake(w, g) :
+    """
+    input: w - word_in_play (string), g - user_input (string)
+    output: bool checks if user guess is wrong
+        if no such g in w, return False, else True
+    """
+    flag = False
+    for i in list(w) :
+        if i == g :
+            flag = True
+    return flag
+
+def check_win(g) :
+    """
+    input: g = template (string)
+    output: bool, if no "_" in g return false else true
+    """
+    for i in list(g) :
+        if i == "_" :
+            return True
+    return False
+def print_result(g) :
+    """
+    input: g - template (string)
+    output: return None, used at just built-in function print(g)
+    """
+    print(f"Вот: {g}")
+def build_template(t, w, g) :
+    """
+    input: t - template (list), w - word (string) g - guess (string)
+    output: t - template (list) with replaced characters in template
     if character in word == guess
-        "character"
-    elif character in template != "_"
         "character"
     else
         "_"
     """
-    i_found_letter = False
     for i in range(len(w)) :
-        if list(w)[i] == g :
-            t[i] = g
-            i_found_letter = True
-        elif t[i] != "_" :
-            t[i] = t[i]
-        else :    
-            t[i] = "_"
-    if i_found_letter == False :
-        l -= 1
-        print(f"У вас осталось {l} попыток")
-    print(list_to_string_convert(t))
-    return [t, l]   
+        if t[i] == "_" :
+            if w[i] == g :
+                t[i] = g
+            else :    
+                t[i] = "_"
+    return t  
     
 def user_input() :
     """
@@ -42,37 +78,41 @@ def start_template(w) :
     for l in w :
         t.append("_")
     return t
-def welcome_speech(t) :
+def welcome_speech(t, l) :
     """
-    input: t - template (string)
+    input: t - template (string), lifes (integer)
     output: return None, it's just usedby built-in function print()    
     """
     print(f'''
         Добро пожаловать в виселицу!
-        Вы должны отгадать слово из {len(t)} букв,
+        Вы должны отгадать слово из {len(t)} букв за {l} жизней,
         иначе вам конец!
         Начинайте: {t}
     ''')
 def get_word(w) :
-    return w[0]
+    import random
+    return w[random.randint(0, len(w) - 1)]
 def game() :
     progress = True
 
-    word = ["iphone"]
-    lifes = 3
+    word = ["iphone", "imac", "ipod", "macintosh", "ipad", "steve", "jobs", "apple"]
     word_in_play = get_word(word)
+    lifes = round(len(word_in_play) / 4 * 3)
     template = start_template(word_in_play)
-    welcome_speech(list_to_string_convert(template))
+    welcome_speech(list_to_string_convert(template), lifes)
    
     while progress : 
         user_guess = user_input()
-        template_and_lifes = build_template(template, word_in_play, user_guess, lifes)
-        template = template_and_lifes[0]
-        lifes = template_and_lifes[1]
-        if list_to_string_convert(template) == word_in_play :
-            print("Вы победили!!!")
-            break
+        template = build_template(template, word_in_play, user_guess)
+        guessed = list_to_string_convert(template)
+        print_result(guessed)
+        progress = check_win(guessed)
+        if not check_mistake(word_in_play, user_guess) :
+            lifes = check_attempt(lifes)
+            print(f"Осталось {lifes} попыток")
         if lifes == 0 :
-            print("Вы проиграли!!!")
+            lose_speech()
             break
+        if not progress :
+            win_speech()
 game()
---
+++ homework-8.py
@@ -0,0 +1,6 @@
+num = int(input("num "))
+for i in range(num) :
+    k = i + 1
+    for j in k :
+        print(str(j) ** (10 ** (i + 1)
+        
